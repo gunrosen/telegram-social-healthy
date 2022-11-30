0805014f1833410532d4f447597dc05c8d4ca4bd
hulk19 <phamhungbkhn@gmail.com>
2022-11-30 17:49:01 +0700

    update: constant

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,6 +1,18 @@
+const UPSERT = {
+  UPDATE: 1,
+  INSERT: 2
+}
 const TELEGRAM_TYPE = {
   CHANNEL: 'channel',
   GROUP: 'group',
 }
 
-export {TELEGRAM_TYPE}
+const TELEGRAM_LANGUAGE = {
+  DEFAULT: 'en',
+  VIETNAMESE: 'vi',
+  FRENCH: 'fr',
+  CHINESE: 'cn',
+  JAPANESE: 'jp',
+  KOREAN: 'kr'
+}
+export {UPSERT, TELEGRAM_TYPE, TELEGRAM_LANGUAGE}
