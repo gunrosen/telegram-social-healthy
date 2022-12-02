a36c72a5d043c8ec1647feca71c7ab48043af641
hulk19 <phamhungbkhn@gmail.com>
2022-12-02 10:25:17 +0700

    update: schema

--- migrations/1669793372595_log-time-series.js
+++ migrations/1669793372595_log-time-series.js
@@ -10,19 +10,31 @@ exports.up = pgm => {
         type: {type: 'varchar(50)', notNull: true, comments: 'Type: channel/group'},
         sub_type: {type: 'varchar(50)', comments: 'SubType: sustainable field'},
 
-        new_message: {type: 'integer'},
-        new_message_by_admin: {type: 'integer'},
-        new_message_by_bot: {type: 'integer'},
+        number_action_chat_add_user: {type: 'integer'},
+        number_action_chat_delete_user: {type: 'integer'},
+        number_action_chat_joined_by_link: {type: 'integer'},
+        number_action_chat_joined_by_request: {type: 'integer'},
+        number_action_pin_message: {type: 'integer'},
 
-        new_reaction: {type: 'integer'},
-        new_view: {type: 'integer'},
-        new_forward: {type: 'integer'},
-        new_reply: {type: 'integer'},
+        number_message: {type: 'integer'},
+        number_message_by_bot: {type: 'integer'},
+        number_message_forward_from_channel: {type: 'integer'},
+        number_media_photo: {type: 'integer'},
+        number_media_document: {type: 'integer'},
+        number_media_poll: {type: 'integer'},
+
+        number_reaction: {type: 'integer'},
+        number_view: {type: 'integer'},
+        number_forward: {type: 'integer'},
+        number_reply: {type: 'integer'},
 
         created_at: {
             type: 'timestamp',
             notNull: true,
             default: pgm.func('current_timestamp'),
+        },
+        compared_old_at: {
+            type: 'timestamp',
         },
     })
 };
